4d03362fbfbc89937071482d02349a7c2c95dcaf
Ardit Nurcaj <arditnurcaj@gmail.com>
2019-04-19 15:01:36 +0200

    Added TableView to display sample documents.

--- SimplePDFApp/ViewController.swift
+++ SimplePDFApp/ViewController.swift
@@ -6,13 +6,18 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var tableView: UITableView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
   
 }
+
+
 
